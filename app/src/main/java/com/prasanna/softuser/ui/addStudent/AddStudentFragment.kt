b203bb131f3342786e5afc5bd0d15136b9e9d430
prasanna-adhikari <prasannaadhikari022@gmail.com>
2021-01-13 14:55:50 +0545

    Binding added in add student fragment

--- app/src/main/java/com/prasanna/softuser/ui/addStudent/AddStudentFragment.kt
+++ app/src/main/java/com/prasanna/softuser/ui/addStudent/AddStudentFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,7 +16,13 @@ import com.prasanna.softuser.R
 class AddStudentFragment : Fragment() {
 
     private lateinit var addStudentViewModel: AddStudentViewModel
-
+    private lateinit var etName: EditText
+    private lateinit var etAge: EditText
+    private lateinit var rdMale: RadioGroup
+    private lateinit var rdFemale: RadioGroup
+    private lateinit var rdOther: RadioGroup
+    private lateinit var etAddress: EditText
+    private lateinit var btnAddStudent: Button
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -21,10 +30,16 @@ class AddStudentFragment : Fragment() {
     ): View? {
         addStudentViewModel =
                 ViewModelProvider(this).get(AddStudentViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_add_student, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        addStudentViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
-        return root
+        val view = inflater.inflate(R.layout.fragment_add_student, container, false)
+        etName = view.findViewById(R.id.etName)
+        etAge = view.findViewById(R.id.etAge)
+        rdMale = view.findViewById(R.id.rdMale)
+        rdFemale = view.findViewById(R.id.rdFemale)
+        rdOther = view.findViewById(R.id.rdOther)
+        etAddress = view.findViewById(R.id.etAddress)
+        btnAddStudent = view.findViewById(R.id.btnAddStudent)
+
+
+        return view
     }
 }
